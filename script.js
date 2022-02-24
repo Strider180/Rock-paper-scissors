7134203da5d08e6baeee940facea1d0f6c2835c5
Peter Huynh <mrpeterhuynh@gmail.com>
2022-02-23 23:48:45 -0800

    set up notes and initial setup

---
+++ script.js
@@ -0,0 +1,22 @@
+// Rock paper scissors game
+// computerPlay function will randomly return 'Rock', 'Paper', 'Scissors'
+// playerSelection vs computerSelection, returns a string that will declare the winner of the round.
+// 5 games will be played
+// Inputs should be case insensitive.
+//use prompt() to get input from the user
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
+
+  for (let i = 0; i < 5; i++) {
+    // your code here!
+ }
+
+ function game() {
+     //Call playRound function.  Play 5 round game and keep score and report the winner or loser at the end.
+ }
